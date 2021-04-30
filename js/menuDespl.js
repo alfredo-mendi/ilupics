a6ea8ecb32728f48429d0e4df9463187f0961f09
alfredo-mendi <82898136+alfredo-mendi@users.noreply.github.com>
2021-04-30 22:12:10 +0200

    Añadida la función altImg() altura de imágenes.

--- js/menuDespl.js
+++ js/menuDespl.js
@@ -10,3 +10,12 @@ function ponQuitaMenu() {
 }
 function ponMenu() { document.getElementById("desplegable").style.display = "block" }
 function quitaMenu() { document.getElementById("desplegable").style.display = "none" }
+function altImg() {
+    var numImg = document.images.length;
+    var altura = document.images[1].naturalHeight;
+    //El contador empieza en 1 porque hay una imagen en el header//
+    for (i = 1; i < numImg; i++) {
+        if (document.images[i].naturalHeight < altura) { altura = document.images[i].naturalHeight }
+    };
+    for (i = 1; i < numImg; i++) { document.images[i].height = altura };
+}
